2902d39cca27ba9ed4f935ecd275744e014d7f75
Arseny Zakharov <myboloto99@gmail.com>
2021-12-22 14:04:45 +0300

    useless defines removed

--- demo_bsp.c
+++ demo_bsp.c
@@ -3,22 +3,6 @@
 #include <stdio.h>
 #include "demo_bsp.h"
 
-#define B1_Pin GPIO_PIN_13
-#define B1_GPIO_Port GPIOC
-#define B1_EXTI_IRQn EXTI15_10_IRQn
-#define USART_TX_Pin GPIO_PIN_2
-#define USART_TX_GPIO_Port GPIOA
-#define USART_RX_Pin GPIO_PIN_3
-#define USART_RX_GPIO_Port GPIOA
-#define LD2_Pin GPIO_PIN_5
-#define LD2_GPIO_Port GPIOA
-#define TMS_Pin GPIO_PIN_13
-#define TMS_GPIO_Port GPIOA
-#define TCK_Pin GPIO_PIN_14
-#define TCK_GPIO_Port GPIOA
-#define SWO_Pin GPIO_PIN_3
-#define SWO_GPIO_Port GPIOB
-
 TIM_HandleTypeDef htim3;
 
 UART_HandleTypeDef huart2;
